f16b22f8d9614df09a6061f9606f7eb13d3df02e
Henry Cooksley <hcooksley@deloitte.co.uk>
2019-04-24 15:37:24 +0100

    Finish part 22

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import {WarningAlertComponent} from './alerts/warning/warning.alert.component';
+import {SuccessAlertComponent} from './alerts/success/success.alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
