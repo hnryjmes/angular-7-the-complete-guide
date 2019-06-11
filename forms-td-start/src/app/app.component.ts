fc2c3fc4bf076f1cc6593cfdc77ef0b540815ef9
Henry Cooksley <hcooksley@deloitte.co.uk>
2019-06-11 11:12:07 +0100

    Finish part 193

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f', { read: false, static: false }) signupForm: NgForm;
   defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
